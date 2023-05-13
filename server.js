fcb57676e7b1d34da9e5083f78a8c91392aee1b5
Atul Sagar <61117347+Atul-Sagar@users.noreply.github.com>
2023-05-13 09:56:33 +0530

    Installed express

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const mysql = require('mysql2')
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) =>{
+    res.send('Hello from express')
+});
+
+app.listen(port, () =>{
+    console.log('Server is running on port ${port}');
+})
 
 const connection = mysql.createConnection({
     host: 'localhost',
